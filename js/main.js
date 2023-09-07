c8af96dbc976e32616881386db32f582687a4de9
Sarah Thomas <sbade92@gmail.com>
2023-09-07 12:39:38 -0700

    Finshed form for now

---
+++ js/main.js
@@ -0,0 +1,11 @@
+Formio.createForm(document.getElementById('formio'), 'https://hikbeodwymyczgq.form.io/physicaltherapyintake');
+
+// const submit = document.querySelectorAll("button");
+// const endScreen = document.querySelector(".end-screen");
+// const formio = document.querySelector("#formio")
+
+
+// submit.addEventListener("click", function(){
+//     formio.classList.add("hide");
+//     endScreen.classList.remove("hide");
+// });
